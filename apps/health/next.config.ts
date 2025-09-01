bf72047bb3c9db1458882b16641694a4db0d89c1
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2025-09-01 18:20:01 +0100

    test: again

--- apps/health/next.config.ts
+++ apps/health/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
-import withTM from "next-transpile-modules";
 
-const withTranspile = withTM(["@repo/ui"]);
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -39,4 +37,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withTranspile(nextConfig);
+export default nextConfig;
