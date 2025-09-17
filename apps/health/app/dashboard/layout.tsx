b979d39b5a2344173c52b5c727f4ba840fe83a0e
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2025-09-17 15:17:56 +0100

    lets go again

--- apps/health/app/dashboard/layout.tsx
+++ apps/health/app/dashboard/layout.tsx
@@ -69,8 +69,8 @@ export default function DashboardLayout({
 
   useEffect(() => {
     const token =
-      // typeof window !== "undefined" ? localStorage.getItem("token") : null;
-      localStorage.getItem("token");
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+      // localStorage.getItem("token");
     if (!token) {
       router.replace("/");
     }
